bc5cc3dff2d9f085759555758e6a1f15d8b087da
shap77 <invshapka@gmail.com>
2026-04-19 12:41:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,3 @@ class Config:
     UPLOAD_FOLDER = 'uploads'
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024   # 10 МБ
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx', 'jpg', 'jpeg', 'png', 'webp'}
-    # JWT использует тот же SECRET_KEY (можно задать отдельный, но необязательно)
-    # JWT_SECRET_KEY = 'very_secret_key'
